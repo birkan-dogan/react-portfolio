49fa72611706d6a2088210012022fce839d8ca02
birkan <b.561birkan@gmail.com>
2022-07-25 02:02:06 +0300

    creating db for looping by map() and showing data

---
+++ src/helpers/Projectlist.jsx
@@ -0,0 +1,52 @@
+import proj1 from "../assets/studied/movie-app.png";
+import proj2 from "../assets/studied/recipe-app.png";
+import proj3 from "../assets/studied/weather-app.png";
+import proj4 from "../assets/studied/randomuser.png";
+import proj5 from "../assets/studied/axios-router.png";
+import proj6 from "../assets/studied/breakingbad.png";
+import proj7 from "../assets/studied/ios.png";
+import proj8 from "../assets/studied/toDoList.png";
+
+export const projectlist = [
+  {
+    name: "Movie App",
+    image: proj1,
+    skills: "React, JavaScript, HTML, CSS, Bootstrap, Axios, Firebase-auth",
+  },
+  {
+    name: "Recipe App",
+    image: proj2,
+    skills:
+      "React, JavaScript, HTML, CSS, Bootstrap, Axios, styled-components, react-router",
+  },
+  {
+    name: "Weather App",
+    image: proj3,
+    skills: "React, JavaScript, HTML, CSS, Axios",
+  },
+  {
+    name: "Random User",
+    image: proj4,
+    skills: "React, JavaScript, HTML, CSS, Bootstrap, Axios",
+  },
+  {
+    name: "GitHub Following",
+    image: proj5,
+    skills: "React, JavaScript, HTML, CSS, Bootstrap, Axios",
+  },
+  {
+    name: "Breaking Bad",
+    image: proj6,
+    skills: "React, JavaScript, HTML, CSS, Axios",
+  },
+  {
+    name: "IOS Calculator",
+    image: proj7,
+    skills: "JavaScript, HTML, CSS",
+  },
+  {
+    name: "ToDo List",
+    image: proj8,
+    skills: "JavaScript, HTML, CSS, Bootstrap",
+  },
+];

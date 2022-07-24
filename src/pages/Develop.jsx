53ee008f5586dc264bd99f00b7c6ab57b730f77c
birkan <b.561birkan@gmail.com>
2022-07-25 00:56:27 +0300

    Develop page is completed but not responsive yet

--- src/pages/Develop.jsx
+++ src/pages/Develop.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { AiFillHtml5 } from "react-icons/ai";
+import { IoLogoJavascript } from "react-icons/io";
+import { MdEngineering } from "react-icons/md";
+import { FaGit } from "react-icons/fa";
+import img1 from "../assets/images/41308287040223.png";
+import img2 from "../assets/images/22310236695384.png";
+import img3 from "../assets/images/image (1).png";
+import img4 from "../assets/images/image (2).png";
 
 const Develop = () => {
-  return <div>Develop</div>;
+  const imgStyle = {
+    width: "450px",
+    borderRadius: "1rem",
+    margin: "1rem",
+  };
+  return (
+    <div className="certificate">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="HTML & CSS"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<AiFillHtml5 />}
+        >
+          <img src={img1} alt="" style={imgStyle} />
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="JavaScript"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<IoLogoJavascript />}
+        >
+          <img src={img2} alt="" style={imgStyle} />
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Git & GitHub"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<FaGit />}
+        >
+          <img src={img3} alt="" style={imgStyle} />
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="Feature Engineering"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<MdEngineering />}
+        >
+          <img src={img4} alt="" style={imgStyle} />
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 };
 
 export default Develop;

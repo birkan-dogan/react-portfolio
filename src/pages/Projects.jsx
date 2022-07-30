b719a33d658c14440971ad35b6760da719985e98
birkan <b.561birkan@gmail.com>
2022-07-30 14:36:39 +0300

    lazy loading in Projects component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ProjectItem from "../components/ProjectItem";
 import { projectlist } from "../helpers/Projectlist";
 import "../styles/projects.css";
 const Projects = () => {
+  const [load, setLoad] = useState(0);
+  let mappingList = projectlist.slice(0, 5);
+  const handleLoad = () => {
+    setLoad(load + 1);
+    console.log(load);
+  };
   return (
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        {projectlist.map((item, index) => {
-          return <ProjectItem id={index} item={item} />;
-        })}
+        {!load
+          ? projectlist.slice(0, 5).map((item, index) => {
+              return <ProjectItem id={index} item={item} />;
+            })
+          : load > 1
+          ? projectlist.slice(0, projectlist.length - 1).map((item, index) => {
+              return <ProjectItem id={index} item={item} />;
+            })
+          : projectlist.slice(0, 10).map((item, index) => {
+              return <ProjectItem id={index} item={item} />;
+            })}
       </div>
+      {load < 2 && (
+        <button onClick={handleLoad} className="btn">
+          Load More
+        </button>
+      )}
     </div>
   );
 };

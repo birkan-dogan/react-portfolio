eb76e90b02677335ff27c31d709c21347b12bbba
birkan <b.561birkan@gmail.com>
2022-07-30 14:01:43 +0300

    adding new projects

--- src/helpers/Projectlist.jsx
+++ src/helpers/Projectlist.jsx
@@ -6,7 +6,13 @@ import proj5 from "../assets/studied/axios-router.png";
 import proj6 from "../assets/studied/breakingbad.png";
 import proj7 from "../assets/studied/ios.png";
 import proj8 from "../assets/studied/toDoList.png";
-
+import proj9 from "../assets/studied/fire-contact.png";
+import proj10 from "../assets/studied/tutorial.png";
+import proj11 from "../assets/studied/guess.png";
+import proj12 from "../assets/studied/bootstrap.png";
+import proj13 from "../assets/studied/netflix.png";
+import proj14 from "../assets/studied/colorFlipper.png";
+import proj15 from "../assets/studied/slideContent.png";
 export const projectlist = [
   {
     name: "Movie App",
@@ -24,11 +30,27 @@ export const projectlist = [
     live: "https://recipe-app-with-styledcomponents.vercel.app/",
   },
   {
+    name: "Fire-Contact",
+    image: proj9,
+    skills:
+      "React, JavaScript, HTML, CSS, Bootstrap, Material UI, firebase-realtime-database,toast-notifications",
+    github: "https://github.com/birkan-dogan/firecontact-app",
+    live: "https://firecontact-app-tau.vercel.app/",
+  },
+  {
     name: "Weather App",
     image: proj3,
     skills: "React, JavaScript, HTML, CSS, Axios",
     github: "https://github.com/birkan-dogan/weather-app",
     live: "https://weather-app-ten-taupe.vercel.app/",
+  },
+  {
+    name: "Add Tutorial",
+    image: proj10,
+    skills:
+      "React, JavaScript, HTML, CSS, Bootstrap, toast-notifications, axios",
+    github: "https://github.com/birkan-dogan/tutorialapp",
+    live: "https://tutorialapp-delta.vercel.app/",
   },
   {
     name: "Random User",
@@ -64,5 +86,40 @@ export const projectlist = [
     skills: "JavaScript, HTML, CSS, Bootstrap",
     github: "https://github.com/birkan-dogan/toDoList",
     live: "https://birkan-dogan.github.io/toDoList/",
+  },
+  {
+    name: "Guess The Number",
+    image: proj11,
+    skills: "JavaScript, HTML, CSS",
+    github: "https://github.com/birkan-dogan/GuessMyNumber",
+    live: "https://holdthenumber.netlify.app/",
+  },
+  {
+    name: "Sass portfolio",
+    image: proj12,
+    skills: "HTML, CSS, Sass",
+    github: "https://github.com/birkan-dogan/portfolio",
+    live: "https://birkan-dogan.github.io/portfolio/index.html",
+  },
+  {
+    name: "Netflix HomePage with HTML,CSS",
+    image: proj13,
+    skills: "HTML, CSS",
+    github: "https://github.com/birkan-dogan/Netflix",
+    live: "https://birkan-dogan.github.io/Netflix/",
+  },
+  {
+    name: "Color Flipper",
+    image: proj14,
+    skills: "HTML, CSS, JavaScript",
+    github: "https://github.com/birkan-dogan/colorFlipper",
+    live: "https://birkan-dogan.github.io/colorFlipper/index.html",
+  },
+  {
+    name: "Slide Content",
+    image: proj15,
+    skills: "HTML, CSS, JavaScript",
+    github: "https://github.com/birkan-dogan/slideContent",
+    live: "https://birkan-dogan.github.io/slideContent/",
   },
 ];
